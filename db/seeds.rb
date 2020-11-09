1bc39ecdc7784ea86e05411bbf57c729903a8fbb
saief124 <saiefsayed123@hotmail.com>
2020-11-09 13:20:51 -0600

    added seed data and relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Location.destroy_all
+Event.destroy_all
+User.destroy_all
+UserEvent.destroy_all
+#Locations
+l1 = Location.create(:city=> "Houston", :state=> "Texas", :zip_code=> 77888)
+
+#Events
+e1 = Event.create(:name=>"Rock Concert", :category=> "Music", :description=>"Linkin Park is playing", :location_id=>l1.id)
+
+#User
+u1 = User.create(:fist_name=>"Adam", :last_name=> "Lambert")
+
+#User Events
+ue1 = UserEvent.create(:user_id=>u1_id, :event_id=>e1_id)
